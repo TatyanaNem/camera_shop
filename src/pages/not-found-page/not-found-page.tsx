2b1b6d6b63788a2510834d68a26b8ddc1fae4de6
Tatyana Nemchina <23angorka07@gmail.com>
2024-01-25 23:16:45 +0300

    fix: added link to not-found-page

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,11 +1,19 @@
 import notFoundPage from './not-found-page.svg';
 import styles from './not-found-page.module.css';
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../common/const';
 
 export function NotFoundPage () {
   return (
     <div className="page-content">
       <div className={styles.wrapper}>
         <img src={notFoundPage} alt={'404'} className={styles.error404} />
+        <Link
+          className="btn btn--purple"
+          to={AppRoute.Root}
+        >
+          Вернуться на главную
+        </Link>
       </div>
     </div>
   );
