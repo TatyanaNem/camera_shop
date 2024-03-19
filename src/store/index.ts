0b7454a523bf79c10c202d1cd8b08a36ca59e1b5
Tatyana Nemchina <23angorka07@gmail.com>
2024-03-19 09:11:18 +0300

    feat: added save of cart state to localStorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createApi } from '../services/api';
 import { rootReducer } from './rootReducer';
+import { loadCartState, saveCartState } from '../utils/local-storage';
+import { NameSpace } from '../common/const';
+import { debounce } from 'ts-debounce';
 
 export const api = createApi();
 
@@ -12,4 +15,14 @@ export const store = configureStore({
         extraArgument: api
       },
     }),
+  preloadedState: {[NameSpace.CartProcess]: loadCartState()}
+});
+
+const debouncedSave = debounce(() => {
+  const state = store.getState();
+  saveCartState(state.CART_PROCESS);
+}, 1000);
+
+store.subscribe(() => {
+  debouncedSave();
 });
---
+++ src/utils/local-storage.ts
@@ -0,0 +1,38 @@
+import { RequestStatus } from '../common/const';
+import { TCartProcess } from '../common/types/state';
+
+const DEFAULT_CART_STATE = {
+  isAddToCartModalOpen: false,
+  product: null,
+  productForRemove: null,
+  camerasInCart: [],
+  isSuccessModalOpen: false,
+  isRemoveFromCartModalOpen: false,
+  promoCode: '',
+  promoCodeSendingStatus: RequestStatus.Idle,
+  discount: null
+};
+
+const CART_STATE_KEY = 'cart-state';
+
+export function loadCartState(): TCartProcess {
+  try {
+    const serializedState: string | null = localStorage.getItem(CART_STATE_KEY);
+    if (!serializedState){
+      return DEFAULT_CART_STATE;
+    } else {
+      return JSON.parse(serializedState) as TCartProcess;
+    }
+  } catch (e) {
+    return DEFAULT_CART_STATE;
+  }
+}
+
+export function saveCartState(state: TCartProcess) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem(CART_STATE_KEY, serializedState);
+  } catch (e) {
+    // ignore error
+  }
+}
